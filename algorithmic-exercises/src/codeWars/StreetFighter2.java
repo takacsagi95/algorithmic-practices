ada103a3bbfa7c0261f3cfc81472a083737cd7be
takacsagi95 <takacsagi95@gmail.com>
2022-10-16 17:30:04 +0200

    CodeWars - Street Fighter 2 done

--- algorithmic-exercises/src/codeWars/StreetFighter2.java
+++ algorithmic-exercises/src/codeWars/StreetFighter2.java
@@ -1,10 +1,9 @@
 package codeWars;
 
-import static java.lang.Math.abs;
-
-import java.util.ArrayList;
+import org.junit.jupiter.api.Test;
 
 public class StreetFighter2 {
+
   /*
   fighters = [
   ["Ryu", "E.Honda", "Blanka", "Guile", "Balrog", "Vega"],
@@ -15,17 +14,12 @@ public class StreetFighter2 {
    */
   public static void main(String[] args) {
     String[][] fighters = new String[][]{
-        new String[] { "Ryu", "E.Honda", "Blanka", "Guile", "Balrog", "Vega" },
-        new String[] { "Ken", "Chun Li", "Zangief", "Dhalsim", "Sagat", "M.Bison" },
+        new String[]{"Ryu", "E.Honda", "Blanka", "Guile", "Balrog", "Vega"},
+        new String[]{"Ken", "Chun Li", "Zangief", "Dhalsim", "Sagat", "M.Bison"},
     };
-    System.out.println("_______________________________________");
-    System.out.println(fighters[0][0]);
-    System.out.println(fighters[1].length);
-    System.out.println(fighters.length);
-    System.out.println(fighters[1][0]);
-    System.out.println("_______________________________________");
-    int[] initial_position = {0,0};
-    String[] moves = new String[] {"up", "left", "right", "left", "left"};
+    int[] initial_position = {0, 0};
+    String[] moves = new String[]{"up", "left", "right", "left", "left"};
+    // result: ['Ryu', 'Vega', 'Ryu', 'Vega', 'Balrog']
     System.out.println(streetFighterSelection(fighters, initial_position, moves));
     for (String string : streetFighterSelection(fighters, initial_position, moves)) {
       System.out.println(string);
@@ -33,36 +27,60 @@ public class StreetFighter2 {
 
   }
 
-  public static String[] streetFighterSelection(String[][] fighters, int[] position, String[] moves){
+  public static String[] streetFighterSelection(String[][] fighters, int[] position,
+      String[] moves) {
     String tempFighter = "";
-    int[] positonAfterStep = {position[0], position[1]};
-    String[] touchedFighter = new String[moves.length];
+    int[] positAfterStep = {position[0], position[1]};
+    String[] touchedFighters = new String[moves.length];
     String[] takenSteps = new String[moves.length];
-    int iters = 0;
-    while (moves.length == takenSteps.length){
-      for (int i = 0; i < fighters.length; i++) {
-        for (int j = 0; j < fighters[0].length; j++) {
-          // Kezdő pozícióból kell elindítani
-          if (moves[iters] == "up"){
-            if (positonAfterStep[i] - 1 < 0) {
-              positonAfterStep[i] = 0;
-              //tempFighter = positonAfterStep[][];
-            }
-          } else if (moves[iters].equals("down")) {
-            if (positonAfterStep[i] + 1 > fighters.length) {
-              positonAfterStep[i] = fighters.length;
-            }
 
-          } else if (moves[iters].equals("right")) {
-
-          }  else if (moves[iters].equals("left")) {
-
-          }
+    for (int i = 0; i < moves.length; i++) {
+      if (moves[i] == "up") {
+        if (positAfterStep[0] == 0) { // position[x,y] -> x = 0 --> we are in the upstream row
+          positAfterStep = positAfterStep; // no change needed on coordinates
+          tempFighter = fighters[positAfterStep[0]][positAfterStep[1]];
+          touchedFighters[i] = tempFighter;
+        } else {
+          positAfterStep[0] = 0;
+          tempFighter = fighters[positAfterStep[0]][positAfterStep[1]];
+          touchedFighters[i] = tempFighter;
+        }
+      } else if (moves[i] == "down") {
+        if (positAfterStep[0] == 1) { // position[x,y] -> x = 0 --> we are in the downstream row
+          positAfterStep = positAfterStep; // no change needed on coordinates
+          tempFighter = fighters[positAfterStep[0]][positAfterStep[1]];
+          touchedFighters[i] = tempFighter;
+        } else {
+          positAfterStep[0] = 1;
+          tempFighter = fighters[positAfterStep[0]][positAfterStep[1]];
+          touchedFighters[i] = tempFighter;
+        }
+      } else if (moves[i] == "left") {
+        // át kell ugrani a lista végére !! végére
+        if (positAfterStep[1] == 0) {
+          positAfterStep[1] = fighters[0].length - 1;
+          tempFighter = fighters[positAfterStep[0]][positAfterStep[1]];
+          touchedFighters[i] = tempFighter;
+        } else {
+          // lehet balra lépkedni
+          positAfterStep[1] -= 1; // for step left the second coordinate should be increment by 1
+          tempFighter = fighters[positAfterStep[0]][positAfterStep[1]];
+          touchedFighters[i] = tempFighter;
+        }
+      } else if (moves[i] == "right") {
+        // át kell ugrani a list ELEJÉRE
+        if (positAfterStep[1] == fighters[0].length - 1) {
+          positAfterStep[1] = 0;
+          tempFighter = fighters[positAfterStep[0]][positAfterStep[1]];
+          touchedFighters[i] = tempFighter;
+        } else {
+          // possible to step right
+          positAfterStep[1] += 1; // for step right the secod coordinate should be increase by 1
+          tempFighter = fighters[positAfterStep[0]][positAfterStep[1]];
+          touchedFighters[i] = tempFighter;
         }
       }
-      takenSteps[iters] = moves[iters];
-      iters++;
     }
-    return touchedFighter;
+    return touchedFighters;
   }
 }
