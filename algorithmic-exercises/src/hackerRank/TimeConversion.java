941a2e67636c58a996cbbea428b9bf07e62b7710
takacsagi95 <takacsagi95@gmail.com>
2022-10-21 13:26:23 +0200

    TimeConversion done

--- algorithmic-exercises/src/hackerRank/TimeConversion.java
+++ algorithmic-exercises/src/hackerRank/TimeConversion.java
@@ -1,7 +1,6 @@
 package hackerRank;
 
 import java.util.HashMap;
-import javax.print.DocFlavor.STRING;
 
 public class TimeConversion {
 
@@ -28,29 +27,46 @@ public class TimeConversion {
       put("01", "13");
       put("00", "12");
     }};
-    for (String str : sArr) {
-      System.out.println(str);
-    }
     String amOrPm = sArr[2].substring(2);
     StringBuilder sb = new StringBuilder();
     for (int i = 0; i < sArr.length; i++) {
       if (amOrPm.equals("AM")) {
         String converted = timesHM.get(sArr[0]);
-        sb = sb.append(converted);
-        sb = sb.append(":");
-        sb = sb.append(sArr[1]);
-        String newTime = sb.toString();
-        System.out.println(newTime);
-
+        sb.append(converted);
+        sb.append(":");
+        sb.append(sArr[1]);
+        sb.append(":");
+        sb.append(sArr[2].substring(0,2));
+        break;
       } else if (amOrPm.equals("PM")) {
-        sb = sb.append(sArr[0]);
-        sb = sb.append(":");
-        sb = sb.append(sArr[1]);
-
+        sb.append(sArr[0]);
+        sb.append(":");
+        sb.append(sArr[1]);
+        sb.append(":");
+        sb.append(sArr[2].substring(0,2));
+        break;
       }
     }
     String newTime = sb.toString();
     System.out.println(newTime);
   }
 
+  // Method with other logic - IN PROGRESS !!! NOT DONE !!!!
+  public static void timeConversionSecond(String s) {
+    String[] sArr = s.split(":");
+    String amOrPm = sArr[2].substring(2);
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < sArr.length; i++) {
+      if (sArr[2].substring(2).equals("AM")) {
+        int hour = Integer.parseInt(String.valueOf(sArr[0]));
+        hour += 12;
+        String hourStr = Integer.toString(hour);
+        sb.append(hourStr);
+        sb.append(":");
+        sb.append(sArr[1]);
+      } else if (sArr[2].substring(2).equals("PM")) {
+
+      }
+    }
+  }
 }
